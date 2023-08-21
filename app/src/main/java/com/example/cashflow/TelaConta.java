1cf554b8899a1e6d8c81a45d849475e920ac5d95
domfernnan <57579248+domfennan@users.noreply.github.com>
2023-08-21 15:34:56 -0300

    autentificação e login + firebase + tela de conta funcionando

--- app/src/main/java/com/example/cashflow/TelaConta.java
+++ app/src/main/java/com/example/cashflow/TelaConta.java
@@ -1,5 +1,6 @@
 package com.example.cashflow;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,11 +9,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class TelaConta extends AppCompatActivity {
 
     private Button bt_deslogar;
     private ImageView bt_voltar;
+
+    private TextView nomeUsuario,emailUsuario;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +39,10 @@ public class TelaConta extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(TelaConta.this, FormLogin.class);
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(TelaConta.this,FormLogin.class);
                 startActivity(intent);
+                finish();
             }
         });
         bt_voltar.setOnClickListener(new View.OnClickListener() {
@@ -38,8 +54,31 @@ public class TelaConta extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    nomeUsuario.setText(documentSnapshot.getString("nome"));
+                    emailUsuario.setText(email);
+                }
+            }
+        });
+    }
+
     private void IniciarComponentes(){
         bt_deslogar = findViewById(R.id.bt_deslogar);
         bt_voltar = findViewById(R.id.bt_voltar);
+        nomeUsuario = findViewById(R.id.textNomeUsuario);
+        emailUsuario = findViewById(R.id.textEmailUsuario);
+
     }
 }
